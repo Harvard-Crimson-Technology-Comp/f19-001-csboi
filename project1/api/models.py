e7b473a3181f9921e3127beec738cc6bcc08c86d
Sim J A Sayer <ssayer@college.harvard.edu>
2019-09-28 21:13:43 -0400

    Submitting a largely uncompleted attempt...

--- project1/api/models.py
+++ project1/api/models.py
@@ -6,18 +6,47 @@ from django.db import models
 class PurchaseModel(models.Model):
     class Meta:
         app_label = 'api'
+        timestamp = models.DateTimeField()
+        user = models.ForeignKey(
+            'UserModel',
+            on_delete=modles.CASCADE
+        )
+        symbol = models.CharField()
+        price = models.FlaotField()
+        quantity = models.IntegerField()
+
 
 # the class containing your user information (including their api token and password, etc)
 class UserModel(models.Model):
     class Meta:
         app_label = 'api'
+        first_name = models.CharField()
+        last_name = models.CharField()
+        api_id = models.UUIDField()
+        password = models.CharField() # Assuming it has already been hashed
+        username = modesl.PrimaryKey()
+        portfolio = models.OnetoOnefield(
+            PortfolioModel,
+            on_delete=modles.CASCADE
 
+        )
 # the class containing a portfolio of stocks
 class PortfolioModel(models.Model):
     class Meta:
         app_label = 'api'
+        cash = models.FloatField()
+        portfolio = models.ForeignKey(
+            'UserModel',
+            on_delete=modles.CASCADE
+        )
 
 # a class containing a simple set of data on a stock within a portfolio
 class StockModel(models.Model):
     class Meta:
+        portfolio = models.ForeignKey(
+            'Portfiolio',
+            on_delete=modles.CASCADE
+        )
         app_label = 'api'
+        symbol = models.CharField()
+        quantity = models.IntegerField()
